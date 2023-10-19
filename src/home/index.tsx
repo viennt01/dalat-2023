0d5b0ac2f0ac6f8fdff587502a7221f257fe3190
viennt01 <thanhviennguyen01@gmail.com>
2023-10-19 09:00:20 +0700

    add name

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,8 +1,50 @@
-import React from 'react';
-// import videoBg from '../components/assets/videoBg.mp4';
+import { animated, useTransition } from '@react-spring/web';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import styles from './styles.module.scss';
 
 export default function Home() {
-  // console.log(videoBg);
+  const ref = useRef<ReturnType<typeof setTimeout>[]>([]);
+  const [items, set] = useState<string[]>([]);
+  const transitions = useTransition(items, {
+    from: {
+      opacity: 0,
+      height: 0,
+      innerHeight: 0,
+      transform: 'perspective(600px) rotateX(0deg)',
+      color: '#8fa5b6',
+    },
+    enter: [
+      { opacity: 1, height: 80, innerHeight: 80 },
+      { transform: 'perspective(600px) rotateX(180deg)', color: '#28d79f' },
+      { transform: 'perspective(600px) rotateX(0deg)' },
+    ],
+    leave: [
+      { color: '#c23369' },
+      { innerHeight: 0 },
+      { opacity: 0, height: 0 },
+    ],
+    update: { color: '#28b4d7' },
+  });
+
+  const reset = useCallback(() => {
+    ref.current.forEach(clearTimeout);
+    ref.current = [];
+    set([]);
+    ref.current.push(
+      setTimeout(() => set(['MÌNH', 'ĐI CHƠi', 'CÙNG NHAU NHÉ!']), 2000)
+    );
+    ref.current.push(
+      setTimeout(() => set(['ĐI CHƠi', 'CÙNG NHAU NHÉ!']), 5000)
+    );
+    ref.current.push(
+      setTimeout(() => set(['MÌNH', 'ĐI CHƠi', 'CÙNG NHAU NHÉ!']), 8000)
+    );
+  }, []);
+
+  useEffect(() => {
+    reset();
+    return () => ref.current.forEach(clearTimeout);
+  }, []);
 
   return (
     <div
@@ -19,7 +61,7 @@ export default function Home() {
           left: '0',
           width: '100%',
           height: '100%',
-          // backgroundColor: 'rgba(0,0,0,.4)',
+          backgroundColor: 'rgba(0,0,0,.4)',
         }}
       ></div>
       <video
@@ -42,8 +84,17 @@ export default function Home() {
           color: 'white',
         }}
       >
-        <h1>Welcome</h1>
-        <p>To my site.</p>
+        {transitions(({ innerHeight, ...rest }, item) => (
+          <animated.div
+            className={styles.transitionsItem}
+            style={rest}
+            onClick={reset}
+          >
+            <animated.div style={{ overflow: 'hidden', height: innerHeight }}>
+              {item}
+            </animated.div>
+          </animated.div>
+        ))}
       </div>
     </div>
   );
